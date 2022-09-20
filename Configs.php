7b58d83969a5e7152499e785b9233911e9bdbbe4
georgethedeveloper77 <georgekarani77@gmail.com>
2022-09-20 22:53:15 +0300

    make it better

---
+++ Configs.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+use Parse\ParseClient;
+use Parse\ParseSessionStorage;
+
+error_reporting(null);
+
+session_start();
+
+try {
+    ParseClient::initialize(
+        'ASNNDrrmNIHFyedPSwEgWiKYzQWnT6J6JRUA98pL',
+        'RAQX4zeCujTIuMU8p20neBYAsBLGwpf5xG7fyl0U',
+        'LpNKoSI7eDnLCYcz1pstHtJJbnuk3mJfv0nQJwbP');
+    ParseClient::setServerURL('https://parseapi.back4app.com/', '/');
+    ParseClient::setStorage(new ParseSessionStorage());
+} catch (Exception $e) {
+}
+
+$health = ParseClient::getServerHealth();
+if ($health['status'] !== 200) {
+}
+
+// Website root url
+$GLOBALS['WEBSITE_PATH'] = 'https://flicker-admin.herokuapp.com;
